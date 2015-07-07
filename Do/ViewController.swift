05cc3474391032de1e5738d2fb7d141e46932066
Jia Jing <jiajing@yangyimac.local>
2015-07-07 20:29:58 +0800

    Initial Commit

---
+++ Do/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Do
+//
+//  Created by Jia Jing on 7/7/15.
+//  Copyright (c) 2015 Jia Jing. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
